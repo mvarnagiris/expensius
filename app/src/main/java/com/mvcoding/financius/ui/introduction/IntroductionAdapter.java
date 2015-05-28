b6ebc80826723d266fd661750519dae32d43c84f
Mantas <mantas.varnagiris@gmail.com>
2015-05-28 23:44:09 +0100

    Introduction WIP

--- app/src/main/java/com/mvcoding/financius/ui/introduction/IntroductionAdapter.java
+++ app/src/main/java/com/mvcoding/financius/ui/introduction/IntroductionAdapter.java
@@ -14,5 +14,43 @@
 
 package com.mvcoding.financius.ui.introduction;
 
-public class IntroductionAdapter {
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mvcoding.financius.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class IntroductionAdapter extends PagerAdapter {
+    private final List<Introduction> introductions;
+
+    public IntroductionAdapter() {
+        introductions = new ArrayList<>();
+        introductions.add(new Introduction(R.drawable.ic_action_chevron_right, R.string.app_name, R.string.app_name));
+        introductions.add(new Introduction(R.drawable.ic_action_chevron_right, R.string.app_name, R.string.app_name));
+        introductions.add(new Introduction(R.drawable.ic_action_chevron_right, R.string.app_name, R.string.app_name));
+    }
+
+    @Override public int getCount() {
+        return introductions.size();
+    }
+
+    @Override public Object instantiateItem(ViewGroup container, int position) {
+        final IntroductionView introductionView = (IntroductionView) LayoutInflater.from(container.getContext())
+                .inflate(R.layout.view_introduction, container, false);
+        introductionView.setIntroduction(introductions.get(position));
+        container.addView(introductionView);
+        return introductionView;
+    }
+
+    @Override public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
 }
