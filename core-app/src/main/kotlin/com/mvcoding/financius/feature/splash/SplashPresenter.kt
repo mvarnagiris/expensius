e32d98e829566f202aa28aa21344b370d32107d5
mvarnagiris <mantas.varnagiris@gmail.com>
2015-10-26 23:29:14 +0000

    Added back core-app module

---
+++ core-app/src/main/kotlin/com/mvcoding/financius/feature/splash/SplashPresenter.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Mantas Varnagiris.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.mvcoding.financius.feature.splash
+
+import com.mvcoding.financius.Session
+import com.mvcoding.financius.UserSettings
+import com.mvcoding.financius.feature.Presenter
+
+class SplashPresenter(private val userSettings: UserSettings, private val session: Session) : Presenter<SplashPresenter.View>() {
+    override fun onAttachView(view: View) {
+        super.onAttachView(view)
+
+        if (session.isLoggedIn() || userSettings.isIntroductionSeen()) {
+            view.startOverview()
+        } else {
+            view.startIntroduction()
+        }
+    }
+
+    interface View : Presenter.View {
+        fun startOverview()
+        fun startIntroduction()
+    }
+}
---
+++ core-app/src/test/kotlin/com/mvcoding/financius/feature/intro/IntroPresenterTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2015 Mantas Varnagiris.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.mvcoding.financius.feature.intro
+
+import com.mvcoding.financius.Session
+import com.mvcoding.financius.UserSettings
+import org.junit.Before
+import org.junit.Test
+import org.mockito.BDDMockito.*
+import rx.subjects.PublishSubject
+
+class IntroPresenterTest {
+    val skipLoginSubject = PublishSubject.create<Any>()
+    val loginSubject = PublishSubject.create<Any>()
+    val session = mock(Session::class.java)
+    val userSettings = mock(UserSettings::class.java)
+    val view = mock(IntroPresenter.View::class.java)
+    val introPages = arrayListOf(IntroPage(), IntroPage(), IntroPage())
+    val presenter = IntroPresenter(introPages, userSettings)
+
+    @Before
+    fun setUp() {
+        given(view.onSkipLogin()).willReturn(skipLoginSubject)
+        given(view.onLogin()).willReturn(loginSubject)
+    }
+
+    @Test
+    fun showsIntroPages() {
+        presenter.onAttachView(view)
+
+        verify(view).showIntroPages(introPages)
+    }
+
+    @Test
+    fun setsIntroductionAsSeenAndStartsOverviewWhenSkippingLogin() {
+        presenter.onAttachView(view)
+
+        skipLogin()
+
+        verify(userSettings).setIsIntroductionSeen(true)
+        verify(view).startOverview()
+    }
+
+    @Test
+    fun setsIntroductionAsSeenAndStartsLoginWhenLoggingIn() {
+        presenter.onAttachView(view)
+
+        login()
+
+        verify(userSettings).setIsIntroductionSeen(true)
+        verify(view).startLogin()
+    }
+
+    private fun login() {
+        loginSubject.onNext(Any())
+    }
+
+    private fun skipLogin() {
+        skipLoginSubject.onNext(Any())
+    }
+}
