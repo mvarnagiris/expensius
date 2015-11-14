505c7e8cc1c57c510b7f0f6a3c6c617ebc4c26b1
Mantas <mantas.varnagiris@gmail.com>
2015-11-14 15:26:01 +0000

    Updated libraries. Added more colors to tagview.

--- app/src/main/kotlin/com/mvcoding/financius/feature/tag/TagView.kt
+++ app/src/main/kotlin/com/mvcoding/financius/feature/tag/TagView.kt
@@ -18,15 +18,26 @@ import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.ColorUtils
+import android.support.v4.graphics.drawable.DrawableCompat
+import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
 import android.util.Pair
 import android.widget.LinearLayout
+import com.jakewharton.rxbinding.view.clicks
+import com.jakewharton.rxbinding.widget.textChanges
+import com.larswerkman.lobsterpicker.ColorAdapter
+import com.larswerkman.lobsterpicker.OnColorListener
+import com.memoizrlabs.ShankModuleInitializer
 import com.mvcoding.financius.R
 import com.mvcoding.financius.extension.provideActivityScopedSingleton
 import com.mvcoding.financius.extension.showSnackbar
 import com.mvcoding.financius.extension.supportsLollipop
 import com.mvcoding.financius.extension.toActivity
 import kotlinx.android.synthetic.view_tag.view.*
+import rx.Observable
 
 class TagView : LinearLayout, TagPresenter.View {
     val presenter by lazy { provideActivityScopedSingleton(TagPresenter::class) }
@@ -47,6 +58,7 @@ class TagView : LinearLayout, TagPresenter.View {
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+        lobsterPicker.colorAdapter = MaterialColorAdapter(context)
         lobsterPicker.addDecorator(lobsterShadeSlider)
     }
 
@@ -165,33 +177,43 @@ class TagView : LinearLayout, TagPresenter.View {
     }
 
     private class MaterialColorAdapter(context: Context) : ColorAdapter {
-        val colors: Array<Int>
+        val colors: Array<IntArray>
 
         init {
             val red = context.resources.getIntArray(R.array.reds)
-            val deepPurple = context.resources.getIntArray(R.array.reds)
-            val lightBlue = context.resources.getIntArray(R.array.reds)
-            val green = context.resources.getIntArray(R.array.reds)
-            val yellow = context.resources.getIntArray(R.array.reds)
-            val deepOrange = context.resources.getIntArray(R.array.reds)
-            val blueGrey = context.resources.getIntArray(R.array.reds)
-            val pink = context.resources.getIntArray(R.array.reds)
-            val indigo = context.resources.getIntArray(R.array.reds)
-            val cyan = context.resources.getIntArray(R.array.reds)
+            val deepPurple = context.resources.getIntArray(R.array.deep_purples)
+            val lightBlue = context.resources.getIntArray(R.array.light_blues)
+            val green = context.resources.getIntArray(R.array.greens)
+            val yellow = context.resources.getIntArray(R.array.yellows)
+            val deepOrange = context.resources.getIntArray(R.array.deep_oranges)
+            val blueGrey = context.resources.getIntArray(R.array.blue_greys)
+            val pink = context.resources.getIntArray(R.array.pinks)
+            val indigo = context.resources.getIntArray(R.array.indigos)
+            val cyan = context.resources.getIntArray(R.array.cyans)
+            val lightGreen = context.resources.getIntArray(R.array.light_greens)
+            val amber = context.resources.getIntArray(R.array.ambers)
+            val brown = context.resources.getIntArray(R.array.browns)
+            val purple = context.resources.getIntArray(R.array.purples)
+            val blue = context.resources.getIntArray(R.array.blues)
+            val teal = context.resources.getIntArray(R.array.teals)
+            val lime = context.resources.getIntArray(R.array.limes)
+            val orange = context.resources.getIntArray(R.array.oranges)
+            val grey = context.resources.getIntArray(R.array.greys)
 
-            colors = arrayOf()
+            colors = arrayOf(red, deepPurple, lightBlue, green, yellow, deepOrange, blueGrey, pink,
+                    indigo, cyan, lightGreen, amber, brown, purple, blue, teal, lime, orange, grey)
         }
 
         override fun size(): Int {
-            throw UnsupportedOperationException()
+            return colors.size
         }
 
         override fun shades(position: Int): Int {
-            throw UnsupportedOperationException()
+            return colors[position].size
         }
 
         override fun color(position: Int, shade: Int): Int {
-            throw UnsupportedOperationException()
+            return colors[position][shade]
         }
     }
 }
