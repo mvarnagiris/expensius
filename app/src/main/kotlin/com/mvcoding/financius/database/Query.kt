22dafbf40dcbdd03959b68b98491ad215e5b90fe
Mantas <mantas.varnagiris@gmail.com>
2015-11-16 10:29:07 +0000

    Updated copyright.

--- app/src/main/kotlin/com/mvcoding/financius/database/Query.kt
+++ app/src/main/kotlin/com/mvcoding/financius/database/Query.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2015 Mantas Varnagiris.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package com.mvcoding.financius.database
 
 fun select(vararg columns: String): Query.Select = Query.Select(columns);
@@ -7,7 +21,5 @@ open class Query {
         fun from(table: String): From = From(this, table)
     }
 
-    class From(private val select: Select, private val table: String) : Query() {
-
-    }
+    class From(private val select: Select, private val table: String) : Query()
 }
