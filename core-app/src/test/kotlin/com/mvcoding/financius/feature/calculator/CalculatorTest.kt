3927eee5194e0fac29de659f80de27d37d9d632b
Mantas <mantas.varnagiris@gmail.com>
2015-12-04 08:59:59 +0000

    Failing tests for Calculator.

---
+++ core-app/src/test/kotlin/com/mvcoding/financius/feature/calculator/CalculatorTest.kt
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2015 Mantas Varnagiris.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.mvcoding.financius.feature.calculator
+
+import org.junit.Test
+
+class CalculatorTest {
+    @Test
+    fun setsExpressionToANumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun clearsExpression() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun deletesLastSymbolFromExpression() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDigitWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDigitWhenExpressionEndsWithNumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDigitWhenExpressionEndsWithDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDigitWhenExpressionEndsWithOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun divideIsIgnoredWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun divideIsIgnoredWhenExpressionIsOnlyAMinusOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun divideReplacesPreviousOperatorWhenExpressionEndsWithOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDivideOperatorWhenExpressionEndsWithNumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDivideOperatorWhenExpressionEndsWithDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun multiplyIsIgnoredWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun multiplyIsIgnoredWhenExpressionIsOnlyAMinusOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun multiplyReplacesPreviousOperatorWhenExpressionEndsWithOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsMultiplyOperatorWhenExpressionEndsWithNumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsMultiplyOperatorWhenExpressionEndsWithDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsSubtractOperatorWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun subtractReplacesPreviousOperatorWhenExpressionEndsWithOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsSubtractOperatorWhenExpressionEndsWithNumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsSubtractOperatorWhenExpressionEndsWithDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addIsIgnoredWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addIsIgnoredWhenExpressionIsOnlyAMinusOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addReplacesPreviousOperatorWhenExpressionEndsWithOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsAddOperatorWhenExpressionEndsWithNumber() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsAddOperatorWhenExpressionEndsWithDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun decimalIsIgnoredWhenExpressionHasOneNumberThatAlreadyHasDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun decimalIsIgnoredWhenExpressionHasMoreThanOneNumberAndLastNumberAlreadyHasDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDecimalWhenExpressionIsEmpty() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDecimalWhenExpressionHasOneNumberWithoutDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDecimalWhenExpressionHasMoreThanOneNumberAndLastOneIsWithoutDecimal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun addsDecimalWhenExpressionEndsWithAnOperator() {
+        throw UnsupportedOperationException()
+    }
+
+    @Test
+    fun calculateEvaluatesExpression() {
+        throw UnsupportedOperationException()
+    }
+}
