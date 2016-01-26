86af058d8393acdbcead0ba3b8208ebf8c670515
mvarnagiris <mantas.varnagiris@gmail.com>
2016-01-26 22:11:48 +0000

    Progressing with query builder.

--- app/src/main/kotlin/com/mvcoding/expensius/provider/database/QueryRequestBuilder.kt
+++ app/src/main/kotlin/com/mvcoding/expensius/provider/database/QueryRequestBuilder.kt
@@ -16,28 +16,71 @@ package com.mvcoding.expensius.provider.database
 
 import com.mvcoding.expensius.provider.database.table.Column
 import com.mvcoding.expensius.provider.database.table.Table
-import com.mvcoding.expensius.provider.database.table.TransactionsTable
 
 interface Element {
-    fun sql(): String
+    fun elementPartSql(): String
 }
 
-abstract class Sql(private var elements: List<Element>) : Element {
-    override fun sql() = elements.joinToString(separator = " ", transform = { it.sql() })
+class Select(private val columns: Array<Column>) : Element {
+    constructor(vararg table: Table) : this(table.map { it.columns() }.flatten().toTypedArray())
+
+    fun from(table: Table) = From(this, columns, table)
+
+    override fun elementPartSql() = "SELECT ${columns.joinToString { it.name }}"
 }
 
-class Select(private vararg val columns: Column) : Sql(emptyList()) {
-    constructor(vararg table: Table) : this(*table.map { it.columns() }.flatten().toTypedArray())
-
-    fun from(table: Table) = From(table)
-
-    override fun sql() = "SELECT ${columns.joinToString { it.name }}"
+abstract class Sql(private val previousElement: Element, val columns: Array<Column>, val tables: Array<Table>) : Element {
+    fun sql() = "${previousElement.elementPartSql()} ${elementPartSql()}"
 }
 
-class From(val table: Table) : Element {
-    override fun sql() = table.name
+abstract class BaseFrom(previousElement: Element, columns: Array<Column>, tables: Array<Table>) : Sql(previousElement, columns, tables) {
+    override fun elementPartSql() = "${keyword()} ${tables.last()}"
+    abstract fun keyword(): String
+    fun leftJoin(table: Table) = LeftJoin(this, columns, tables.plus(table))
+    fun where(clause: String) = Where(this, columns, tables, "WHERE", clause)
 }
 
-fun test() {
-    Select(TransactionsTable()).from()
+class From(previousElement: Element, columns: Array<Column>, table: Table) : BaseFrom(previousElement, columns, arrayOf(table)) {
+    override fun keyword() = "FROM"
+}
+
+class LeftJoin(previousElement: Element, columns: Array<Column>, tables: Array<Table>) : BaseFrom(previousElement, columns, tables) {
+    override fun keyword() = "LEFT JOIN"
+    fun on(clause: String) = On(this, columns, tables, clause)
+}
+
+class On(
+        previousElement: Element,
+        columns: Array<Column>,
+        tables: Array<Table>,
+        private val clause: String) : BaseFrom(previousElement, columns, tables) {
+
+    override fun elementPartSql(): String {
+        return "${keyword()} $clause"
+    }
+
+    override fun keyword() = "ON"
+}
+
+class From2(
+        previousElement: Element,
+        columns: Array<Column>,
+        table: Table) : Sql(previousElement, columns, arrayOf(table)) {
+
+    override fun elementPartSql() = "FROM ${tables.last().name}"
+}
+
+class Join
+
+class Where(
+        previousElement: Element,
+        columns: Array<Column>,
+        tables: Array<Table>,
+        private val keyword: String,
+        private val clause: String) : Sql(previousElement, columns, tables) {
+
+    override fun elementPartSql() = "$keyword $clause"
+
+    fun and(clause: String) = Where(this, columns, tables, "AND", clause)
+    fun or(clause: String) = Where(this, columns, tables, "OR", clause)
 }
