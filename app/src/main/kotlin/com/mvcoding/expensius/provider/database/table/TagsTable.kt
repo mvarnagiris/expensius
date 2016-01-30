9d82fb40c9d6d06a8dd17791e2d3325110955f7d
Mantas <mantas.varnagiris@gmail.com>
2016-01-30 12:41:13 +0000

    Extracted tags separators to constants.

--- app/src/main/kotlin/com/mvcoding/expensius/provider/database/table/TagsTable.kt
+++ app/src/main/kotlin/com/mvcoding/expensius/provider/database/table/TagsTable.kt
@@ -18,11 +18,16 @@ import com.mvcoding.expensius.provider.database.table.ValueColumn.Type.INTEGER
 import com.mvcoding.expensius.provider.database.table.ValueColumn.Type.TEXT
 
 class TagsTable : ModelTable("tags") {
+    companion object {
+        const val COLUMN_SEPARATOR = ":!:"
+        const val TAG_SEPARATOR = "?:?"
+    }
+
     val title = ValueColumn(this, "title", TEXT);
     val color = ValueColumn(this, "color", INTEGER, "0");
 
     val transactionTags = CalculatedColumn(
-            "group_concat(${columns().joinToString(separator = ":;:", transform = { it.toString() })}, ';:;')",
+            "group_concat(${columns().joinToString(separator = COLUMN_SEPARATOR, transform = { it.toString() })}, '$TAG_SEPARATOR')",
             "tags")
 
     override fun modelColumns() = arrayOf(title, color)
