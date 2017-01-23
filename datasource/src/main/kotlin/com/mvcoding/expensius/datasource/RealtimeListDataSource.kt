9158d0a9c87ba7c6461f0c6c67ac0b91706a8279
Mantas <mantas.varnagiris@gmail.com>
2017-01-23 17:49:33 +0000

    Handling error appearance.

--- datasource/src/main/kotlin/com/mvcoding/expensius/datasource/RealtimeListDataSource.kt
+++ datasource/src/main/kotlin/com/mvcoding/expensius/datasource/RealtimeListDataSource.kt
@@ -16,34 +16,83 @@ package com.mvcoding.expensius.datasource
 
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.AddedItems
+import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.AllItems
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.ChangedItems
-import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.CurrentItems
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.MovedItem
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.RemovedItems
 import rx.Observable
+import rx.Observable.just
+import rx.Subscriber
 import rx.lang.kotlin.deferredObservable
+import rx.lang.kotlin.filterNotNull
 import rx.lang.kotlin.observable
+import java.lang.Math.max
+import java.lang.Math.min
 
 class RealtimeListDataSource<ITEM>(
-        private val currentItems: () -> Observable<CurrentItems<ITEM>>,
-        private val addedItems: () -> Observable<AddedItems<ITEM>>,
-        private val changedItems: () -> Observable<ChangedItems<ITEM>>,
-        private val removedItems: () -> Observable<RemovedItems<ITEM>>,
-        private val movedItem: () -> Observable<MovedItem<ITEM>>) : DataSource<RealtimeData<ITEM>> {
+        private val getAllItems: () -> Observable<AllItems<ITEM>>,
+        private val getAddedItems: () -> Observable<AddedItems<ITEM>>,
+        private val getChangedItems: () -> Observable<ChangedItems<ITEM>>,
+        private val getRemovedItems: () -> Observable<RemovedItems<ITEM>>,
+        private val getMovedItem: () -> Observable<MovedItem<ITEM>>) : DataSource<RealtimeData<ITEM>> {
 
-    override fun data(): Observable<RealtimeData<ITEM>> = deferredObservable {
+    private var items: List<ITEM>? = null
+
+    private val observable = deferredObservable {
         observable<RealtimeData<ITEM>> { subscriber ->
-            val currentItems = currentItems().first()
-            currentItems.subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
-            addedItems().skipUntil(currentItems).subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
-            changedItems().skipUntil(currentItems).subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
-            removedItems().skipUntil(currentItems).subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
-            movedItem().skipUntil(currentItems).subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
+            val allItems = getAllItems().first()
+            handleAllItems(allItems, subscriber)
+            handleAddedItems(allItems, subscriber)
+            handleChangedItems(allItems, subscriber)
+            handleRemovedItems(allItems, subscriber)
+            handleMovedItems(allItems, subscriber)
         }
+    }.share()
+
+    private fun handleAllItems(allItems: Observable<AllItems<ITEM>>, subscriber: Subscriber<in RealtimeData<ITEM>>) {
+        allItems.doOnNext { items = it.items }.subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
     }
 
+    private fun handleAddedItems(allItems: Observable<AllItems<ITEM>>, subscriber: Subscriber<in RealtimeData<ITEM>>) {
+        getAddedItems()
+                .skipUntil(allItems)
+                .doOnNext { items = items.orEmpty().insert(it.items, it.position) }
+                .subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
+    }
+
+    private fun handleChangedItems(allItems: Observable<AllItems<ITEM>>, subscriber: Subscriber<in RealtimeData<ITEM>>) {
+        getChangedItems().skipUntil(allItems)
+                .doOnNext { items = items.orEmpty().replace(it.items, it.position) }
+                .subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
+    }
+
+    private fun handleRemovedItems(allItems: Observable<AllItems<ITEM>>, subscriber: Subscriber<in RealtimeData<ITEM>>) {
+        getRemovedItems()
+                .skipUntil(allItems)
+                .doOnNext { items = items.orEmpty().remove(it.items, it.position) }
+                .subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
+    }
+
+    private fun handleMovedItems(allItems: Observable<AllItems<ITEM>>, subscriber: Subscriber<in RealtimeData<ITEM>>) {
+        getMovedItem()
+                .skipUntil(allItems)
+                .doOnNext { items = items.orEmpty().move(it.fromPosition, it.toPosition) }
+                .subscribe({ subscriber.onNext(it) }, { subscriber.onError(it) })
+    }
+
+    override fun data(): Observable<RealtimeData<ITEM>> = observable.mergeWith(currentState())
+
+    private fun currentState() = just(items).filterNotNull().map { AllItems(it) }
+
+    private fun <T> List<T>.insert(items: List<T>, position: Int) = take(position).plus(items).plus(takeLast(size - position))
+    private fun <T> List<T>.replace(items: List<T>, position: Int) = take(position).plus(items).plus(takeLast(max(size - position - items.size, 0)))
+    private fun <T> List<T>.remove(items: List<T>, position: Int) = take(position).plus(takeLast(max(size - position - items.size, 0)))
+    private fun <T> List<T>.move(fromPosition: Int, toPosition: Int) = take(min(fromPosition, toPosition))
+            .plus(if (toPosition <= fromPosition) listOf(get(fromPosition)) else slice((fromPosition + 1)..toPosition))
+            .plus(if (toPosition <= fromPosition) slice(toPosition..(fromPosition - 1)) else listOf(get(fromPosition)))
+
     sealed class RealtimeData<out ITEM> {
-        data class CurrentItems<out ITEM>(val items: List<ITEM>) : RealtimeData<ITEM>()
+        data class AllItems<out ITEM>(val items: List<ITEM>) : RealtimeData<ITEM>()
         data class AddedItems<out ITEM>(val items: List<ITEM>, val position: Int) : RealtimeData<ITEM>()
         data class ChangedItems<out ITEM>(val items: List<ITEM>, val position: Int) : RealtimeData<ITEM>()
         data class RemovedItems<out ITEM>(val items: List<ITEM>, val position: Int) : RealtimeData<ITEM>()
--- datasource/src/test/kotlin/com/mvcoding/expensius/datasource/RealtimeListDataSourceTest.kt
+++ datasource/src/test/kotlin/com/mvcoding/expensius/datasource/RealtimeListDataSourceTest.kt
@@ -16,11 +16,13 @@ package com.mvcoding.expensius.datasource
 
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.AddedItems
+import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.AllItems
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.ChangedItems
-import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.CurrentItems
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.MovedItem
 import com.mvcoding.expensius.datasource.RealtimeListDataSource.RealtimeData.RemovedItems
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Before
 import org.junit.Test
@@ -30,22 +32,24 @@ import rx.observers.TestSubscriber
 
 class RealtimeListDataSourceTest {
 
-    val currentItemsSubject = PublishSubject<CurrentItems<Int>>()
+    val currentItemsSubject = PublishSubject<AllItems<Int>>()
     val addedItemsSubject = PublishSubject<AddedItems<Int>>()
     val changedItemsSubject = PublishSubject<ChangedItems<Int>>()
     val removedItemsSubject = PublishSubject<RemovedItems<Int>>()
     val movedItemSubject = PublishSubject<MovedItem<Int>>()
 
-    val items = listOf(1, 2, 3)
-    val otherItems = listOf(4, 5, 6)
+    val items = listOf(2, 4, 6)
 
-    val currentItems = mock<() -> Observable<CurrentItems<Int>>>()
+    val currentItems = mock<() -> Observable<AllItems<Int>>>()
     val addedItems = mock<() -> Observable<AddedItems<Int>>>()
     val changedItems = mock<() -> Observable<ChangedItems<Int>>>()
     val removedItems = mock<() -> Observable<RemovedItems<Int>>>()
     val movedItem = mock<() -> Observable<MovedItem<Int>>>()
     val realTimeListDataSource = RealtimeListDataSource(currentItems, addedItems, changedItems, removedItems, movedItem)
     val subscriber = TestSubscriber<RealtimeData<Int>>()
+    val otherSubscriber = TestSubscriber<RealtimeData<Int>>()
+    val otherSubscriber2 = TestSubscriber<RealtimeData<Int>>()
+    val otherSubscriber3 = TestSubscriber<RealtimeData<Int>>()
 
     @Before
     fun setUp() {
@@ -67,13 +71,13 @@ class RealtimeListDataSourceTest {
         subscriber.assertNoValues()
 
         receiveCurrentItems(items)
-        subscriber.assertValue(CurrentItems(items))
+        subscriber.assertValue(AllItems(items))
 
         receiveAddedItems(items, 0)
         receiveChangedItems(items, 0)
         receiveMovedItem(1, fromPosition = 0, toPosition = 1)
         receiveRemovedItems(items, 0)
-        subscriber.assertValues(CurrentItems(items), AddedItems(items, 0), ChangedItems(items, 0), MovedItem(1, 0, 1), RemovedItems(items, 0))
+        subscriber.assertValues(AllItems(items), AddedItems(items, 0), ChangedItems(items, 0), MovedItem(1, 0, 1), RemovedItems(items, 0))
     }
 
     @Test
@@ -81,22 +85,101 @@ class RealtimeListDataSourceTest {
         realTimeListDataSource.data().subscribe(subscriber)
 
         receiveCurrentItems(items)
-        receiveCurrentItems(otherItems)
+        receiveCurrentItems(listOf(1, 3))
 
-        subscriber.assertValue(CurrentItems(items))
+        subscriber.assertValue(AllItems(items))
     }
 
     @Test
-    fun `asonly takes first result from current values`() {
+    fun `does not subscribe to current items more than once`() {
         realTimeListDataSource.data().subscribe(subscriber)
+        realTimeListDataSource.data().subscribe(otherSubscriber)
 
         receiveCurrentItems(items)
-        receiveCurrentItems(otherItems)
+        realTimeListDataSource.data().subscribe(otherSubscriber2)
 
-        subscriber.assertValue(CurrentItems(items))
+        subscriber.assertValue(AllItems(items))
+        otherSubscriber.assertValue(AllItems(items))
+        otherSubscriber2.assertValue(AllItems(items))
+        verify(currentItems, times(1)).invoke()
     }
 
-    fun receiveCurrentItems(items: List<Int>) = currentItemsSubject.onNext(CurrentItems(items))
+    @Test
+    fun `handles added items`() {
+        val addedItems1 = listOf(1)
+        val addedItems2 = listOf(3)
+        val addedItems3 = listOf(5)
+        realTimeListDataSource.data().subscribe(subscriber)
+        receiveCurrentItems(items)
+
+        receiveAddedItems(addedItems1, position = 0)
+        realTimeListDataSource.data().subscribe(otherSubscriber)
+
+        receiveAddedItems(addedItems2, position = 2)
+        realTimeListDataSource.data().subscribe(otherSubscriber2)
+
+        receiveAddedItems(addedItems3, position = 4)
+        realTimeListDataSource.data().subscribe(otherSubscriber3)
+
+        subscriber.assertValues(AllItems(items), AddedItems(addedItems1, 0), AddedItems(addedItems2, 2), AddedItems(addedItems3, 4))
+        otherSubscriber.assertValues(AllItems(listOf(1, 2, 4, 6)), AddedItems(addedItems2, 2), AddedItems(addedItems3, 4))
+        otherSubscriber2.assertValues(AllItems(listOf(1, 2, 3, 4, 6)), AddedItems(addedItems3, 4))
+        otherSubscriber3.assertValues(AllItems(listOf(1, 2, 3, 4, 5, 6)))
+    }
+
+    @Test
+    fun `handles changed items`() {
+        val changedItems = listOf(1, 3)
+        realTimeListDataSource.data().subscribe(subscriber)
+        receiveCurrentItems(items)
+
+        receiveChangedItems(changedItems, position = 1)
+        realTimeListDataSource.data().subscribe(otherSubscriber)
+
+        subscriber.assertValues(AllItems(items), ChangedItems(changedItems, 1))
+        otherSubscriber.assertValues(AllItems(listOf(2, 1, 3)))
+    }
+
+    @Test
+    fun `handles removed items`() {
+        val removedItems1 = listOf(4)
+        val removedItems2 = listOf(6)
+        val removedItems3 = listOf(2)
+        realTimeListDataSource.data().subscribe(subscriber)
+        receiveCurrentItems(items)
+
+        receiveRemovedItems(removedItems1, position = 1)
+        realTimeListDataSource.data().subscribe(otherSubscriber)
+
+        receiveRemovedItems(removedItems2, position = 1)
+        realTimeListDataSource.data().subscribe(otherSubscriber2)
+
+        receiveRemovedItems(removedItems3, position = 0)
+        realTimeListDataSource.data().subscribe(otherSubscriber3)
+
+        subscriber.assertValues(AllItems(items), RemovedItems(removedItems1, 1), RemovedItems(removedItems2, 1), RemovedItems(removedItems3, 0))
+        otherSubscriber.assertValues(AllItems(listOf(2, 6)), RemovedItems(removedItems2, 1), RemovedItems(removedItems3, 0))
+        otherSubscriber2.assertValues(AllItems(listOf(2)), RemovedItems(removedItems3, 0))
+        otherSubscriber3.assertValues(AllItems(listOf()))
+    }
+
+    @Test
+    fun `handles moved items`() {
+        realTimeListDataSource.data().subscribe(subscriber)
+        receiveCurrentItems(items)
+
+        receiveMovedItem(2, fromPosition = 0, toPosition = 2)
+        realTimeListDataSource.data().subscribe(otherSubscriber)
+
+        receiveMovedItem(2, fromPosition = 2, toPosition = 0)
+        realTimeListDataSource.data().subscribe(otherSubscriber2)
+
+        subscriber.assertValues(AllItems(items), MovedItem(2, 0, 2), MovedItem(2, 2, 0))
+        otherSubscriber.assertValues(AllItems(listOf(4, 6, 2)), MovedItem(2, 2, 0))
+        otherSubscriber2.assertValues(AllItems(listOf(2, 4, 6)))
+    }
+
+    fun receiveCurrentItems(items: List<Int>) = currentItemsSubject.onNext(AllItems(items))
     fun receiveAddedItems(items: List<Int>, position: Int) = addedItemsSubject.onNext(AddedItems(items, position))
     fun receiveChangedItems(items: List<Int>, position: Int) = changedItemsSubject.onNext(ChangedItems(items, position))
     fun receiveRemovedItems(items: List<Int>, position: Int) = removedItemsSubject.onNext(RemovedItems(items, position))
